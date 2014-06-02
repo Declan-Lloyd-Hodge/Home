e06312d5d1a7f669fb8dcaaab9b6a41408992b96
Declan-Lloyd-Hodge <dec20056@hotmail.com>
2014-06-02 03:53:09 +0100

    Update diablo.js

--- diablo.js
+++ diablo.js
@@ -5,12 +5,12 @@ function HideShowSection(input){
     $(input).children('h3').each(function(){
       $(this).html(str.replace(">+<",">-<"));
     });
-    $(input).parent().parent().css({'border-bottom':'solid 1px #f60','box-shadow':'0px 3px 5px -3px rgba(85,0,0,1)'});
+    $(input).parent().parent().css({'border-bottom':'solid 1px #500','box-shadow':'0px 3px 5px -3px rgba(85,0,0,1)'});
   }else{
     $(input).parent().parent().children('.content').slideUp();
     $(input).children('h3').each(function(){
       $(this).html(str.replace(">-<",">+<"));
     });
-    $(input).parent().parent().css({'border-bottom':'solid 0px #f60','box-shadow':'0px 0px 0px -3px rgba(85,0,0,0)'});
+    $(input).parent().parent().css({'border-bottom':'solid 0px #500','box-shadow':'0px 0px 0px -3px rgba(85,0,0,0)'});
   }
 }
