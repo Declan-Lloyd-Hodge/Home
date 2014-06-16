952b6fc3fb6feae9a7f591103e333dc7520ee543
Declan-Lloyd-Hodge <dec20056@hotmail.com>
2014-06-16 18:01:44 +0100

    Update default.js

--- default.js
+++ default.js
@@ -11,7 +11,13 @@ $(document).ready(function(){
     return val.split('=');
   });
   
-  $('#phone').html(split[0][1]);
-  $('#email').html(split[1][1]);
-  $('#address').html(split[2][1]);
+  if(split[0][1]){
+    $('#phone').html(split[0][1]);
+  }
+  if(split[1][1]){
+    $('#email').html(split[1][1]);
+  }
+  if(split[2][1]){
+    $('#address').html(split[2][1]);
+  }
 });
