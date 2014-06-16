ae2708fdf1828093b72230f87e8ee25e83367d04
Declan-Lloyd-Hodge <dec20056@hotmail.com>
2014-06-17 00:45:15 +0100

    Update default.js

--- default.js
+++ default.js
@@ -20,4 +20,5 @@ $(document).ready(function(){
   if(split[2] && split[2][1]){
     $('#address').html(split[2][1].replace(/-/g, ' ').replace(/_/g, '<br/>'));
   }
+  $('#header').css('top','-100px');
 });
