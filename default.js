6151a1a389102cff09ce4401a132f717cdf385fa
Declan-Lloyd-Hodge <dec20056@hotmail.com>
2014-06-16 18:28:17 +0100

    Update default.js

--- default.js
+++ default.js
@@ -18,7 +18,6 @@ $(document).ready(function(){
     $('#email').html(split[1][1]);
   }
   if(split[2] && split[2][1]){
-    $('#address').html(split[2][1].replace('-', ' '));
-    $('#address').html($('#address').html().replace('_', '<br/>'));
+    $('#address').html(split[2][1].replace(/-/g, ' ').replace(/_/g, '<br/>'));
   }
 });
